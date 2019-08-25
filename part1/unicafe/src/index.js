9fbe06101894ce972d995df523b370376ba80700
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2019-08-25 17:41:04 +0300

    Excersises 1.10 and 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,33 +2,50 @@
  *  2019-08-26
  *
  *  Helsinki Fullstack Mooc
- *  Exercise 1.6 to 1.9
+ *  Exercise 1.6 to 1.11
  */
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import React, {useState} from 'react'; import ReactDOM from 'react-dom';
+
+const Button = ({text, cb}) => {
+    return (
+        <button onClick={cb}>{text}</button>
+    )
+}
+
+const Statistic = ({text, value}) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    )
+}
 
 const Statistics = ({good, neutral, bad}) => {
     const count = good + neutral + bad
 
     if(count < 1)
-    return (
-        <div>
-            <h1>statistics</h1>
-            <p>No feedback give</p>
-        </div>
-    )
+        return (
+            <div>
+                <h1>statistics</h1>
+                <p>No feedback give</p>
+            </div>
+        )
     else
-    return (
-        <div>
-            <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {count}</p>
-            <p>avarage {(good - bad) / count}</p>
-            <p>positive {100*good / count} %</p>
-        </div>
-    )
+        var percentage = 100*good / count + "%"
+        return (
+            <div>
+                <h1>statistics</h1>
+                <table>
+                <Statistic text="good" value={good} />
+                <Statistic text="neutral" value={neutral} />
+                <Statistic text="bad" value={bad} />
+                <Statistic text="all" value={count} />
+                <Statistic text="average" value={(good - bad) / count} />
+                <Statistic text="positive" value={percentage} />
+                </table>
+            </div>
+        )
 }
 
 const App = () => {
@@ -40,9 +57,9 @@ const App = () => {
         <div>
         <h1>give feedback</h1>
         <p>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad+ 1)}>bad</button>
+        <Button text="good" cb={() => setGood(good + 1)} />
+        <Button text="neutral" cb={() => setNeutral(neutral + 1)} />
+        <Button text="bad" cb={() => setBad(bad+ 1)} />
         </p>
         <Statistics good={good} bad={bad} neutral={neutral}/>
         </div>
