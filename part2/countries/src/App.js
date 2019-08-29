4b5097cd121a75db65b4ec831566735131d9b285
Joonatan Kuosa <joonatan.kuosa@gmail.com>
2019-08-29 13:16:02 +0300

    Exercise 2.13: Countries frontend
    Allows searching for countries by name and displays info about said
    country.

---
+++ part2/countries/src/App.js
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const API_URL = 'https://restcountries.eu/rest/v2/name/'
+
+const Country = ({ country }) => {
+    return (
+        <div>
+        <h2>{country.name}</h2>
+        <p>capital: {country.capital}</p>
+        <p>population: {country.population}</p>
+        <h3>languages</h3>
+        <li>{country.lang.map(x => <ul key={x}>{x}</ul>)}</li>
+        <img src={country.flag} height='128' />
+        </div>
+    )
+}
+
+const Countries = ({countries}) => {
+    const matches = countries.length
+
+    const translateCountry = (c) => {
+        const lang = c.languages.map(l => l.name)
+        return {
+            name: c.name,
+            capital: c.capital,
+            lang: lang,
+            population: c.population,
+            flag: c.flag
+        }
+    }
+
+    return (
+        <div>
+        {matches === 1 && <Country country={translateCountry(countries[0])} /> }
+        {matches > 1 && matches <  10
+                && <li>{countries.map(c => <ul key={c.name}>{c.name}</ul>)}</li>}
+        {matches >= 10 && <p>Too many matches, specify another filter</p>}
+        <br />
+        <p>Info: matches {matches}</p>
+        </div>
+    )
+}
+const App = () => {
+    const [filter, setFilter] = useState('')
+    const [countries, setCountries] = useState([])
+
+
+    useEffect(() => {
+        if (filter.length > 0) {
+            axios.get(API_URL.concat(filter))
+                .then(xs => setCountries(xs.data))
+        }
+        // TODO should we retrive the full list or not?
+    }, [filter])
+
+    const filterChangeCb = (event) => {
+        setFilter(event.target.value)
+    }
+
+    return (
+    <div className="App">
+        <p>find countries
+            <input value={filter} onChange={filterChangeCb} />
+        </p>
+        <Countries countries={countries} />
+    </div>
+    )
+}
+
+export default App;
